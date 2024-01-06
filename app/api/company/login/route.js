bf6e93ee301acadcc55a630d091a07f7a969516d
Prayash Mishra <mishraprayash11@gmail.com>
2024-01-06 09:58:36 +0545

    Updated Company Login Route

--- app/api/company/login/route.js
+++ app/api/company/login/route.js
@@ -10,11 +10,9 @@ export async function POST(request) {
         if (!email || !password) {
             return NextResponse.json({ msg: "Invalid Credentials" });
         }
-        const companyExist = await Company.findOne({
-            email: email
-        })
+        const companyExist = await Company.findOne({ $and: [{ email: email }, { state: "Approved" }] })
         if (!companyExist) {
-            return NextResponse.json({ msg: "Invalid Creddentials" })
+            return NextResponse.json({ msg: "Invalid Credentials" })
         }
         // checking if password is macthed.
         const isPasswordMatched = await bcrypt.compare(password, companyExist.password);
@@ -22,14 +20,13 @@ export async function POST(request) {
         if (!isPasswordMatched) {
             return NextResponse.json({ msg: "Invalid Creddentials" });
         }
-       
+
         const token = Company.createJWT();
-        const response = NextResponse.json({ msg: "Successful Login", success: true}, { status: 200});
+        const response = NextResponse.json({ msg: "Successful Login", success: true }, { status: 200 });
 
         // setting jwt token in cookies
         response.cookies.set("token", token, { httpOnly: true });
         return response;
-
     } catch (error) {
         console.log(error);
         return NextResponse.json({ msg: "Error, check server for error logs" });
