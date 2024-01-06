5c03545810fa1d797f0a11a68e17fdb69db65546
Prayash Mishra <mishraprayash11@gmail.com>
2024-01-06 09:55:27 +0545

    Upated Common Routes

---
+++ app/api/common/explore/[id]/route.js
@@ -0,0 +1,16 @@
+import Internship from "@/models/Internship";
+import connectDB from "@/config/database";
+import { NextResponse } from "next/server"
+
+export async function GET(request, { params }) {
+    try {
+        await connectDB();
+        const internship = await Internship.findOne({ _id: params.id, state: "Approved" });
+        if (!internship) {
+            return NextResponse.json({ msg: "No Internship available" }, { status: 200 })
+        }
+        return NextResponse.json({ Internship: internship }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ msg: "Internal Server Error" }, { status: 500 })
+    }
+}
--- app/api/common/logout/route.js
+++ app/api/common/logout/route.js
@@ -1,19 +1,11 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        const response = NextResponse.json({
-            message: "Logout Success",
-            success: true,
-        });
-        response.cookies.set("token", "", {
-            httpOnly: true,
-            expires: new Date(0),
-        });
+        const response = NextResponse.json({ msg: "Logout Success", success: true, });
+        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
         return response;
     } catch (error) {
-        return NextResponse.json({
-            error: error.message
-        })
+        return NextResponse.json({ msg: "Internal Server Error" }, { status: 500 });
     }
 }
