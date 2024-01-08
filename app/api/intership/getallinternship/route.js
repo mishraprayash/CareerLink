d0ce226d07a9c39362e6098c2e68ee991f51283f
bikkibro <bibektimilsina7857@gmail.com>
2024-01-08 18:01:45 +0545

    getintership

--- app/api/intership/getallinternship/route.js
+++ app/api/intership/getallinternship/route.js
@@ -1,11 +1,15 @@
 import { NextResponse } from "next/server";
 import { connectToDB } from '@/utils/connecttodb';
 import Internship from "@/models/Internship";
-
+import { getSession } from "next-auth/react";
 export async function GET(request) {
     try {
         await connectToDB();
-
+        const session = await getSession({ request });
+        if (!session) {
+          console.log("signin required")
+        }
+        console.log(session)
         const allInternships = await Internship.find({});
 
         return NextResponse.json({
