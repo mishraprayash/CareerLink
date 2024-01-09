a50d0954bf8fd59e0dd5b2cf6f4f965e6b81e864
Prayash Mishra <mishraprayash11@gmail.com>
2024-01-10 00:02:52 +0545

    just started

--- middleware.js
+++ middleware.js
@@ -1,20 +1,32 @@
 import { NextResponse } from 'next/server'
 
 
-// const publicRoutes = ['/', '/explore', '/login', '/register'];
+const publicRoutes = ['/', '/explore', '/login', '/register', '/api/admin/login', '/api/admin/register','/api/common/companies','/api/common/explore','/api/company/login','/api/company/register','/api/company/forgotpassword','/api/company/resetpassword'];
 
 // const adminRoutes = ['/admin/:path*']
 // const companyRoutes = ['/company/:path*']
 // const studentRoutes = ['/student/:path*']
 
 
+
+/*handled only for JWT token for admin and company
+havenot actually authenticated as which user is admin or company or student*/
+
 export async function middleware(request) {
-  // const { pathname } = request.nextUrl;
-  // const nexttoken = request.cookies.get('next-auth.session-token') || "";
-  // const token = request.cookies.get('token') || "";
+  const { pathname } = request.nextUrl;
+  console.log(pathname);
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+  const token = request.cookies.get('token');
+  if (!token) {
+    return NextResponse.json({ msg: "Unauthenticated User from middleware" }, { status: 400 });
+  }
   return NextResponse.next();
 
 }
-export const config = {
-  matcher: ['/']
-}
+
+
+// export const config = {
+//   matcher: ['/']
+// }
