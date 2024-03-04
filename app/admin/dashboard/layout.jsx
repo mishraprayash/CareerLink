51479720d6e6cc4baa3257208ddc1b30a70098c8
Prayash Mishra <mishraprayash11@gmail.com>
2024-03-05 03:22:27 +0545

    simple update

--- app/admin/dashboard/layout.jsx
+++ app/admin/dashboard/layout.jsx
@@ -1,20 +1,17 @@
-import { Inter } from 'next/font/google'
-import Sidebar from './components/Sidebar'
 
-const inter = Inter({ subsets: ['latin'] })
+import Sidebar from './components/Sidebar'
 
 export default function DashboardLayout({ children }) {
   return (
     <section className='flex'>
       <div className='w-30'>
-        <Sidebar />
+        <Sidebar/>
       </div>
       <div className="wrapper flex-1 mx-4 flex flex-col min-vh-100 bg-light">
         <div className="wrapper d-flex flex-column min-vh-100 bg-light w-full">
           <div className="body flex-grow-1 px-3">
             {children}
           </div>
-          {/* footer */}
         </div>
       </div>
     </section>
