a5d0c0394aa9d6ebcb90091b39c6388fbcea3760
Prayash Mishra <mishraprayash11@gmail.com>
2024-03-05 01:32:27 +0545

    updated margin,padding and button alignment

--- app/dashboard/company/createinternship/page.jsx
+++ app/dashboard/company/createinternship/page.jsx
@@ -1,11 +1,11 @@
-'use client'
-import { ToastMessage } from '@/app/components/ToastMessage';
-import { postReq } from '@/app/hooks/service';
-import React, { useState,  useContext } from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import { ToastMessage } from "@/app/components/ToastMessage";
+import { postReq } from "@/app/hooks/service";
+import React, { useState, useContext } from "react";
+import { useRouter } from "next/navigation";
 
 const CreateInternship = () => {
- const [createinternship,setCreateInternship]=useState(null)
+  const [createinternship, setCreateInternship] = useState(null);
 
   const [formData, setFormData] = useState({
     position: "",
@@ -22,24 +22,23 @@ const CreateInternship = () => {
     noofVacancy: 1,
   });
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setCreateInternship(formData);
-    console.log(createinternship)
-const response=await postReq("/api/company/createinternship",createinternship)
-console.log(response)
-if(!response.error){
-
-    ToastMessage("Success",response.msg)
-    const router=useRouter()
-    router.push('/dashboard/company/internship')
-}
-else{
-    console.log(response.error)
-    ToastMessage("Error",response.msg)
-
-}
-
+    console.log(createinternship);
+    const response = await postReq(
+      "/api/company/createinternship",
+      createinternship
+    );
+    console.log(response);
+    if (!response.error) {
+      ToastMessage("Success", response.msg);
+      const router = useRouter();
+      router.push("/dashboard/company/internship");
+    } else {
+      console.log(response.error);
+      ToastMessage("Error", response.msg);
+    }
   };
 
   const handleChange = (e) => {
@@ -49,13 +48,19 @@ else{
       [name]: value,
     });
   };
- 
+
   return (
     <div className=" mx-auto w-[80%] p-5 my-5">
-      <div className='text-3xl text-center  text-[#108A00]'> Create New Internship </div>
+      <div className="text-3xl text-center  text-[#108A00]">
+        {" "}
+        Create New Internship{" "}
+      </div>
       <form onSubmit={handleSubmit} className="mt-8 space-y-6">
-        <div className='w-1/2'>
-          <label htmlFor="position" className=" text-sm font-medium text-gray-700">
+        <div className="w-1/2">
+          <label
+            htmlFor="position"
+            className=" text-sm font-medium text-gray-700"
+          >
             Position
           </label>
           <input
@@ -68,8 +73,11 @@ else{
           />
         </div>
 
-        <div  className='w-1/2 '> 
-          <label htmlFor="location" className=" text-sm font-medium text-gray-700">
+        <div className="w-1/2 ">
+          <label
+            htmlFor="location"
+            className=" text-sm font-medium text-gray-700"
+          >
             Location
           </label>
           <input
@@ -82,69 +90,88 @@ else{
           />
         </div>
 
-        
-<div className="flex flex-row items-center justify-center">
-        <div className='w-1/2'>
-          <label htmlFor="workTime" className=" text-sm font-medium text-gray-700">
-            Work Time
-          </label>
-          <select
-            id="workTime"
-            name="workTime"
-            onChange={handleChange}
-            value={formData.workTime}
-            className="mt-1 p-2 border rounded-md w-full"
+        <div className="flex flex-row items-center justify-center">
+          <div className="w-1/2">
+            <label
+              htmlFor="workTime"
+              className=" text-sm font-medium text-gray-700"
+            >
+              Work Time
+            </label>
+            <select
+              id="workTime"
+              name="workTime"
+              onChange={handleChange}
+              value={formData.workTime}
+              className="mt-1 p-2 border rounded-md w-full"
+            >
+              <option value="Full-Time">Full-Time</option>
+              <option value="Part-Time">Part-Time</option>
+            </select>
+          </div>
+
+          <div className="w-1/2  flex flex-col items-center ml-2 ">
+            <label
+              htmlFor="isRemote"
+              className=" text-sm mx-2 font-medium text-gray-700"
+            >
+              Remote
+            </label>
+            <input
+              type="checkbox"
+              id="isRemote"
+              name="isRemote"
+              onChange={(event) =>
+                setFormData({
+                  ...formData,
+                  [event.target.name]: event.target.checked,
+                })
+              }
+              checked={formData.isRemote}
+              className="mt-1 p-2 border rounded-md"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row items-baseline justify-center ">
+          <div className="w-1/2">
+            <label
+              htmlFor="startDate"
+              className=" text-sm font-medium text-gray-700"
+            >
+              Start Date
+            </label>
+            <input
+              type="date"
+              id="startDate"
+              name="startDate"
+              onChange={handleChange}
+              value={formData.startDate}
+              className="mt-1 p-2 border rounded-md w-full"
+            />
+          </div>
+
+          <div className="w-1/2 ">
+            <label
+              htmlFor="endDate"
+              className=" text-sm font-medium text-gray-700"
+            >
+              End Date
+            </label>
+            <input
+              type="date"
+              id="endDate"
+              name="endDate"
+              onChange={handleChange}
+              value={formData.endDate}
+              className="mt-1 p-2 border rounded-md w-full"
+            />
+          </div>
+        </div>
+        <div className="w-1/2">
+          <label
+            htmlFor="internshipType"
+            className=" text-sm font-medium text-gray-700"
           >
-            <option value="Full-Time">Full-Time</option>
-            <option value="Part-Time">Part-Time</option>
-          </select>
-        </div>
-
-        <div className='w-1/2  flex flex-col items-center ml-2 '>
-          <label htmlFor="isRemote" className=" text-sm mx-2 font-medium text-gray-700">
-            Remote
-          </label>
-          <input
-            type="checkbox"
-            id="isRemote"
-            name="isRemote"
-            onChange={(event) => setFormData({ ...formData, [event.target.name]: event.target.checked })}
-        checked={formData.isRemote}
-            className="mt-1 p-2 border rounded-md"
-          />
-        </div>
-        </div>
-          <div className='flex flex-row items-baseline justify-center '>
-        <div className='w-1/2'>
-          <label htmlFor="startDate" className=" text-sm font-medium text-gray-700">
-            Start Date
-          </label> 
-          <input
-            type="date"
-            id="startDate"
-            name="startDate"
-            onChange={handleChange}
-            value={formData.startDate}
-            className="mt-1 p-2 border rounded-md w-full"
-          />
-        </div>
-
-        <div className='w-1/2 '>
-          <label htmlFor="endDate" className=" text-sm font-medium text-gray-700">
-            End Date
-          </label>
-          <input
-            type="date"
-            id="endDate"
-            name="endDate"
-            onChange={handleChange}
-            value={formData.endDate}
-            className="mt-1 p-2 border rounded-md w-full"
-          />
-        </div>
-        </div>
-        <div className='w-1/2'>
-          <label htmlFor="internshipType" className=" text-sm font-medium text-gray-700">
             Internship Type
           </label>
           <select
@@ -160,9 +187,12 @@ else{
           </select>
         </div>
 
-        {formData.internshipType === 'Paid' && (
-           <div className='w-1/2'>
-            <label htmlFor="salary" className="block text-sm font-medium text-gray-700">
+        {formData.internshipType === "Paid" && (
+          <div className="w-1/2">
+            <label
+              htmlFor="salary"
+              className="block text-sm font-medium text-gray-700"
+            >
               Salary
             </label>
             <input
@@ -176,9 +206,12 @@ else{
           </div>
         )}
 
-        {formData.internshipType === 'Paid' && (
-         <div className='w-1/2'>
-            <label htmlFor="noofVacancy" className="block text-sm font-medium text-gray-700">
+        {formData.internshipType === "Paid" && (
+          <div className="w-1/2">
+            <label
+              htmlFor="noofVacancy"
+              className="block text-sm font-medium text-gray-700"
+            >
               Number of Vacancies
             </label>
             <input
@@ -187,11 +220,15 @@ else{
               name="noofVacancy"
               onChange={handleChange}
               value={formData.noofVacancy}
-              className="w-full"/>
-           </div>   
-           )}
+              className="w-full"
+            />
+          </div>
+        )}
         <div>
-          <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="description"
+            className="block text-sm font-medium text-gray-700"
+          >
             Description
           </label>
           <textarea
@@ -204,7 +241,10 @@ else{
         </div>
 
         <div>
-          <label htmlFor="responsibilities" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="responsibilities"
+            className="block text-sm font-medium text-gray-700"
+          >
             Responsibilities
           </label>
           <textarea
@@ -217,7 +257,10 @@ else{
         </div>
 
         <div>
-          <label htmlFor="requirements" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="requirements"
+            className="block text-sm font-medium text-gray-700"
+          >
             Requirements
           </label>
           <textarea
@@ -228,12 +271,14 @@ else{
             className="mt-1 p-2 border rounded-md w-full"
           />
         </div>
-         <div className="flex justify-center">
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded-md text-center flex items-center">
-          Create Internship
-        </button>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-2 rounded-md text-center flex items-center"
+          >
+            Create Internship
+          </button>
         </div>
-       
       </form>
     </div>
   );
