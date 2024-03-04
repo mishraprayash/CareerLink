ea43f4f2f25ae43bbfc681e1e38994744f06d249
Prayash Mishra <mishraprayash11@gmail.com>
2024-03-05 03:41:01 +0545

    simple style change

--- app/admin/dashboard/components/profile/page.jsx
+++ app/admin/dashboard/components/profile/page.jsx
@@ -1,27 +1,20 @@
-'use client'
-import React, { useContext } from 'react';
-import { AuthContext } from '@/app/context/authcontext';
+"use client";
+import React, { useContext } from "react";
+import { AuthContext } from "@/app/context/authcontext";
 
 const Profile = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className='flex justify-center items-center h-fit'>
-    {user ? (
-      user.admin ? (
-        <div className="p-4 bg-gray-200 rounded shadow-md">
-          <div className="font-bold text-lg mb-2">Admin Profile</div>
-          <div className="mb-2">Username: {user.admin.username}</div>
-          <div>Email: {user.admin.email}</div>
+    <div className="flex justify-center items-center my-5 w-full p-5 ">
+      {user && user.admin && (
+        <div className="p-4 rounded shadow-md shadow-black">
+          <div className="font-bold text-lg mb-2 text-center">Admin Profile</div>
+          <div className="my-2 font-semibold text-center">Username: {user.admin.username}</div>
+          <div className="my-2 font-semibold text-center">Email: {user.admin.email}</div>
         </div>
-      ) : (
-        <div className="text-center">Loading data...</div>
-      )
-    ) : (
-      <div className="text-center">You must be logged in</div>
-    )}
-  </div>
-  
+      )}
+    </div>
   );
 };
 
